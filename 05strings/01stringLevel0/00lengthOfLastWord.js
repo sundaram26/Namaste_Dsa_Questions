87234de1ac6e14a988e759feb89155d997f0be9e
sundaram26 <singhsundaram2004@gmail.com>
2025-07-14 00:22:55 +0530

    Length Of Last Word Leetcode-58(Approach 1)

---
+++ 05strings/01stringLevel0/00lengthOfLastWord.js
@@ -0,0 +1,28 @@
+//Length of Last Word (Leetcode - 58)
+//I/P:- s = "Hello world"
+//O/P:- 5 -----> because world has 5 letter
+
+
+function lengthOfLastWord(string) {
+    s = string.trim();
+    s = s.split(" ");
+    
+    return s[s.length - 1].length;
+}
+
+function lengthOfLastWord2(string) {
+    let i = string.length - 1;
+    let count = 0;
+    while (i >= 0 && string[i] === ' ') i--;
+    while (i >= 0 && string[i] !== ' ') {
+        count++;
+        i--;
+    }
+
+    return count;
+}
+
+
+const str = "Hello World";
+console.log("Length of Last Word with built in function: ", lengthOfLastWord(str))
+console.log("Length of Last Word: ", lengthOfLastWord2(str))
