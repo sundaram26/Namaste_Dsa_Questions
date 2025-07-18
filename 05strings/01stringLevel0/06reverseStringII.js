4ab082abd498bda08192279e8747a1ad84292c97
sundaram26 <singhsundaram2004@gmail.com>
2025-07-18 09:33:00 +0530

    corrected logic of reverse string II leetcode - 541

--- 05strings/01stringLevel0/06reverseStringII.js
+++ 05strings/01stringLevel0/06reverseStringII.js
@@ -5,10 +5,11 @@
 function reverseStringII(s, k) {
     s = s.split("");
     for (let i = 0; i < s.length; i = i + (2 * k)){
+        let n = Math.min(k, s.length - i);
         for (let j = 0; j < Math.floor(k/2); j++){
             let temp = s[j + i];
-            s[j + i] = s[k + i - j - 1];
-            s[k + i - j - 1] = temp;
+            s[j + i] = s[n + i - j - 1];
+            s[n + i - j - 1] = temp;
         }
     }
     s = s.join("")
