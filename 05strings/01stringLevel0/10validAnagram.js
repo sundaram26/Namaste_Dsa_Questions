ca5d625c5c5f99cfdfa80f47b5f2cbbc9c22b13c
sundaram26 <singhsundaram2004@gmail.com>
2025-07-26 10:15:03 +0530

    Valid Anagram Leetcode - 242

--- 05strings/01stringLevel0/10validAnagram.js
+++ 05strings/01stringLevel0/10validAnagram.js
@@ -5,10 +5,34 @@
 
 
 function isAnagram(s, t) {
-    
+    return s.split("").sort().join() === t.split("").sort().join();
+}
+
+function isAnagramMap(s, t) {
+    if (s.length !== t.length) return false;
+
+    const map = {};
+
+    for (let i = 0; i < s.length; i++){
+        if (!map[s[i]]) {
+            map[s[i]] = 1;
+        } else {
+            map[s[i]]++;
+        }
+    }
+
+    for (let i = 0; i < t.length; i++){
+        if (!map[t[i]] || map[t[i]] === 0) return false;
+        if (map[t[i]]) {
+            map[t[i]]--;
+        }
+    }
+
+    return true;
 }
 
 const s = "anagram"
 const t = "nagaram"
 
 console.log(isAnagram(s, t));
+console.log(isAnagramMap(s, t));
